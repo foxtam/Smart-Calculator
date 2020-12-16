9dc311a92fbd2f691f94a19f8325fb9ae335f622
Foxtam <foxtam7@ya.ru>
2020-12-16 22:51:53 +0300

    Add substraction

--- Smart Calculator/task/src/calculator/Main.java
+++ Smart Calculator/task/src/calculator/Main.java
@@ -14,17 +14,45 @@ public class Main {
             } else if (input.equals("/help")) {
                 System.out.println("The program calculates the sum of numbers");
             } else if (!input.isBlank()) {
-                int sum = getSum(input);
+                int sum = calculate(input);
                 System.out.println(sum);
             }
         }
     }
 
-    private static int getSum(String input) {
-        String[] numbers = input.split("\\s+");
+    private static int calculate(String input) {
+        input = removePluses(input);
+        input = removeDoubleMinuses(input);
+        String[] inputNumbers = splitIntoNumbers(input);
+        int[] numbers = stringsToNumbers(inputNumbers);
+        return sum(numbers);
+    }
+
+    private static String removePluses(String string) {
+        return string.replaceAll("\\+", "");
+    }
+
+    private static String removeDoubleMinuses(String string) {
+        return string.replaceAll("--", "");
+    }
+
+    private static String[] splitIntoNumbers(String input) {
+        input = input.replaceAll("(?<=-)\\s*(?=\\d)", "");
+        return input.split("\\s+");
+    }
+
+    private static int[] stringsToNumbers(String[] inputNumbers) {
+        int[] array = new int[inputNumbers.length];
+        for (int i = 0; i < inputNumbers.length; i++) {
+            array[i] = Integer.parseInt(inputNumbers[i]);
+        }
+        return array;
+    }
+
+    private static int sum(int[] numbers) {
         int sum = 0;
-        for (String n : numbers) {
-            sum += Integer.parseInt(n);
+        for (int n : numbers) {
+            sum += n;
         }
         return sum;
     }
