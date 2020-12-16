3459b1108d16ccf5b33c3a1b43cbbb527fa03a54
Foxtam <foxtam7@ya.ru>
2020-12-16 15:03:33 +0300

    Add Remove extra spaces task

---
+++ Problems/Remove extra spaces/src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class RemoveExtraSpacesProblem {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+        String normal = text.replaceAll("\\s+", " ");
+        System.out.println(normal);
+    }
+}
