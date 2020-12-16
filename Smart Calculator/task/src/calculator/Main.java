4ccdc4f3781a3c01e36df6feee4878bc4dc46af8
Foxtam <foxtam7@ya.ru>
2020-12-16 23:10:39 +0300

    Add 5/8 Error_

--- Smart Calculator/task/src/calculator/Main.java
+++ Smart Calculator/task/src/calculator/Main.java
@@ -4,41 +4,62 @@ import java.util.Scanner;
 
 public class Main {
 
+    private static boolean doContinue = true;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        while (true) {
+        while (doContinue) {
             String input = scanner.nextLine();
-            if (input.equals("/exit")) {
-                System.out.println("Bye!");
-                break;
-            } else if (input.equals("/help")) {
-                System.out.println("The program calculates the sum of numbers");
+            if (isCommand(input)) {
+                runCommand(input);
             } else if (!input.isBlank()) {
-                int sum = calculate(input);
-                System.out.println(sum);
+                runCalculating(input);
             }
         }
     }
 
-    private static int calculate(String input) {
-        input = removePluses(input);
-        input = removeDoubleMinuses(input);
-        String[] inputNumbers = splitIntoNumbers(input);
+    private static boolean isCommand(String line) {
+        return line.startsWith("/");
+    }
+
+    private static void runCommand(String line) {
+        if (line.equals("/exit")) {
+            System.out.println("Bye!");
+            doContinue = false;
+        } else if (line.equals("/help")) {
+            System.out.println("The program calculates the sum of numbers");
+        } else {
+            System.out.println("Unknown command");
+        }
+    }
+
+    private static void runCalculating(String line) {
+        if (line.matches("[-+]?\\d+(\\s+[-+]+\\s+\\d+)*")) {
+            System.out.println(calculate(line));
+        } else {
+            System.out.println("Invalid expression");
+        }
+    }
+
+    private static int calculate(String line) {
+        line = removePluses(line);
+        line = removeDoubleMinuses(line);
+        String[] inputNumbers = splitIntoNumbers(line);
         int[] numbers = stringsToNumbers(inputNumbers);
         return sum(numbers);
     }
 
-    private static String removePluses(String string) {
-        return string.replaceAll("\\+", "");
+    private static String removePluses(String line) {
+        return line.replaceAll("\\+", "");
     }
 
-    private static String removeDoubleMinuses(String string) {
-        return string.replaceAll("--", "");
+    private static String removeDoubleMinuses(String line) {
+        return line.replaceAll("--", "");
     }
 
-    private static String[] splitIntoNumbers(String input) {
-        input = input.replaceAll("(?<=-)\\s*(?=\\d)", "");
-        return input.split("\\s+");
+    private static String[] splitIntoNumbers(String line) {
+        line = line.replaceAll("(?<=-)\\s*(?=\\d)", "");
+        return line.split("\\s+");
     }
 
     private static int[] stringsToNumbers(String[] inputNumbers) {
